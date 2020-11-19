e83b2fef6386c22a7480161862b4223226dcded3
Yarin <yarin.greenfeld@gmail.com>
2020-11-19 20:55:30 +0200

    extract to main

--- arduino/serial_example1.py
+++ arduino/serial_example1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import serial
-if __name__ == '__main__':
+def main():
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.flush()
     while True:
         if ser.in_waiting > 0:
-             line = ser.readline().decode('utf-8').rstrip()
-             print(line)
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+if __name__ == '__main__':
+    main() 
