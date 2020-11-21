905597a6424558981be5534e3fa48640fdc2f8f5
Yarin <yarin.greenfeld@gmail.com>
2020-11-21 22:55:09 +0200

    for the new times

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -13,10 +13,8 @@
 #include "serialib.h"
 #include <stdio.h>
 #include <iostream>
+#include "arduino.h"
 #define BANDWITH 9600
-#if defined(_WIN32) || defined(_WIN64)
-#define SERIAL_PORT "COM5"
-#endif
 #ifdef __linux__
 #define SERIAL_PORT "/dev/ttyUSB0"
 #endif
@@ -26,26 +24,42 @@
  * \return      0 : success
  *              <0 : an error occured
  */
+
 int main(/*int argc, char *argv[]*/)
 {
-  // Serial object
-  serialib serial;
 
-  // Connection to serial port
-  char errorOpening = serial.openDevice(SERIAL_PORT, BANDWITH);
-
-  // If connection fails, return the error code otherwise, display a success message
-  if (errorOpening != 1)
-    return errorOpening;
-  std::cout << "Successful connection to " << SERIAL_PORT << std::endl;
-
-  char str[64] = {0};
-  int err = serial.readString(str, '!', 64);
-  std::cout << str << std::endl
-            << "err: " << err << std::endl;
-
-  // Close the serial device
-  serial.closeDevice();
-
+  Arduino a;
+  a.openSerial();
+  //a.serialCommend("201");
+  a.addFile("../dataFromArduino/sensor1.log");
+  a.writeFromBufferToFile("hello nigi",0);
   return 0;
 }
+
+// int main(/*int argc, char *argv[]*/)
+// {
+
+//   serialib serial;
+//   // Arduino a =("outputfile.txt","/dev/ttyUSB0",9600,10000);
+//   // Serial object
+//   Arduino a;
+
+//   // Connection to serial port
+//   char errorOpening = serial.openDevice(SERIAL_PORT, BANDWITH);
+//   a.checkConnectionToConsule();
+//   a.checkSum("abc");
+//   // If connection fails, return the error code otherwise, display a success message
+//   if (errorOpening != 1)
+//     return errorOpening;
+//   std::cout << "Successful connection to " << SERIAL_PORT << std::endl;
+
+//   char str[64] = {0};
+//   int err = serial.readString(str, '!', 64);
+//   std::cout << str << std::endl
+//             << "err: " << err << std::endl;
+
+//   // Close the serial device
+//   serial.closeDevice();
+
+//   return 0;
+// }
