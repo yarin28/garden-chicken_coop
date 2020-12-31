b922a279ac7905bac4f415338709e65c5a784cd9
Yarin <yarin.greenfeld@gmail.com>
2020-12-31 21:02:34 +0200

    forgot the main

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -30,7 +30,7 @@ int main(/*int argc, char *argv[]*/)
   Arduino a("/dev/ttyUSB0", BANDWITH, 100000);
   a.openSerial();
   a.addLogFilesForSensors();
-  std::thread rvc = a.startTheArdCheking();
+  std::thread rvc = a.startTheArduinoCheking();
   //std::thread rcv(Arduino::ref(recive));
   // std::thread rcv2 = a.getDataWithWhileLoop();
   // std::thread rcv3(a.getDataWithWhileLoop());
